fd598e324847a4c9f4615f4b7ebbab9cd409baff
Dmytro-Soia <dmytro.soia@jobtrek.ch>
2025-03-18 13:00:34 +0100

    feat: basics exercices is finished

--- src/Basics.php
+++ src/Basics.php
@@ -19,6 +19,7 @@ class Basics
      */
     public static function add(int $number1, int $number2): int
     {
+        return $number1 + $number2;
     }
 
     /**
@@ -26,6 +27,7 @@ class Basics
      */
     public static function length(string $str): int
     {
+        return mb_strlen($str);
     }
 
     /**
@@ -33,6 +35,11 @@ class Basics
      */
     public static function condition(string $str): bool
     {
+        if (mb_strlen($str) > 10) {
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
@@ -40,6 +47,7 @@ class Basics
      */
     public static function concatenate(string $str1, string $str2): string
     {
+        return $str1 . $str2;
     }
 
     /**
@@ -48,6 +56,16 @@ class Basics
      */
     public static function getWordsToCount(string $str, int $wordsCountToRemain): string
     {
+        $array = explode(" ", $str);
+        if (count($array) > $wordsCountToRemain) {
+            do {
+            array_pop($array);
+            } while (count($array) > $wordsCountToRemain);
+        } else if (count($array) < $wordsCountToRemain) {
+            do {
+            array_push($array, end($array));
+            } while (count($array) < $wordsCountToRemain);
+        }
+        return implode(" ", $array);
     }
-
 }
